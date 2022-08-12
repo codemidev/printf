a22cb02d828fbce35c441bafc7a9578b875f719d
Jay_codes <ochaijay81@gmail.com>
2022-08-12 04:48:34 -0700

    functions2

--- test_files/printf.c
+++ test_files/printf.c
@@ -21,41 +21,39 @@ int _printf(const char *format, ...)
 			count += _putchar(format[i]);
 			i++;
 		}
-	  else if (format[i] == '%' && format[i + 1] !=' ')
-	  {
-      switch (format[i + 1])
-      {
-	      case 'c':
-		      
-		      /* print the character from the va_arguments */
-		      count += _putchar(va_arg(data, int));
-		      break;
-	      case 's':
-		      count += print_string(va_arg(data, char *));
-		      break;
-	      case '%':
-		      /* print the character from the va_arguments */
-		      count += _putchar('%');
-		      break;
-	      case 'd':
-            count += print_decimal(va_arg(data, int));
-	    break;
-	      case 'i':
-	    count += print_decimal(va_arg(data, int));
-	    break;
-	      case 'b':
-	    count += print_binary(va_arg(data, int));
-	    break;
-	      case 'u':
-            count += print_unsigned(va_arg(data, unsigned int));
-	    break;
-	      default:
-	    break;
-      }
+		else if (format[i] == '%' && format[i + 1] != ' ')
+		{
+			switch (format[i + 1]);
+			{
+				case 'c':
+					/* print the character from the va_arguments */
+					count += _putchar(va_arg(data, int));
+					break;
+				case 's':
+					count += print_string(va_arg(data, char *));
+					break;
+				case '%':
+					/* print the character from the va_arguments */
+					count += _putchar('%');
+					break;
+				case 'd':
+					count += print_decimal(va_arg(data, int));
+					break;
+				case 'i':
+					count += print_decimal(va_arg(data, int));
+					break;
+				case 'b':
+					count += print_binary(va_arg(data, int));
+					break;
+				case 'u':
+					count += print_unsigned(va_arg(data, unsigned int));
+					break;
+				default:
+					break;
+			}
 
-      i += 2;
-	  }
-  }
-
-  return (count);
+			i += 2;
+		}
+	}
+	return (count);
 }
